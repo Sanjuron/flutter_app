109a85ff1719e42b4650e7850fe2e298fed25d12
Sanjuron <julien_nn@live.fr>
2020-10-22 11:34:56 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp( // MaterialApp est une sorte de wrapper pour le reste de l'app
+      home: Text('Salut!'), // on sépare les propriétés par des virgules // sur la home screen se trouve un texte
+
+  ));
+}
